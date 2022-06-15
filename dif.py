87efd4bb32e6c1440bc965c0d7fdc40d85fee213
HardSkillet <43767465+HardSkillet@users.noreply.github.com>
2022-06-15 11:00:22 +0300

    Initial commit

---
+++ dif.py
@@ -0,0 +1,31 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+m = float(input())
+p = float(input())
+k = float(input())
+
+def f1(y1, y):
+    return -p/m * y1 - k/m*y
+def f2(y):
+    return -p/m*y - (y**2)*k/(2*m)
+
+answer = np.empty(100000)
+variable = np.empty(100000)
+
+h = 0.00001
+y1 = 1
+y = 1
+x = 0
+for i in range(100000):
+    dy1 = h*f1(y1, y)
+    dy = h*f2(y)
+    y += dy
+    y1 += dy1
+    x += h
+    answer[i] = y
+    variable[i] = x
+
+plt.plot(variable, answer)
+plt.show()
