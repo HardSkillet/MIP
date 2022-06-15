4a463202b83080fa2f93472a46ffa16ecb0cbd14
HardSkillet <43767465+HardSkillet@users.noreply.github.com>
2022-06-15 12:23:50 +0300

    fixes

--- dif.py
+++ dif.py
@@ -8,8 +8,6 @@ k = float(input())
 
 def f1(y1, y):
     return -p/m * y1 - k/m*y
-def f2(y, dy1):
-    return -(m/p)*dy1 - (k/p)*y
 
 answer = np.empty(100000)
 variable = np.empty(100000)
@@ -20,7 +18,7 @@ y = 1
 x = 0
 for i in range(100000):
     dy1 = h*f1(y1, y)
-    dy = h*f2(y, dy1)
+    dy = h*y1
     y += dy
     y1 += dy1
     x += h
